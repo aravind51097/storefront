aa86ee5c044b422ed817ed118618a86f09723d2d
aravind51097 <aravind51097@gmail.com>
2021-11-16 19:26:16 +0530

    commit

--- store/serializer.py
+++ store/serializer.py
@@ -1,15 +1,38 @@
-from rest_framework import serializers
+from .models import Collection
+from django.db.models.query import QuerySet
+from rest_framework import fields, serializers
 from decimal import Decimal
 
 from store.models import Product
 
+from store import models
 
-class ProductSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
-    price=serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id','title']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','slug','description','inventory','unit_price','tax','collection']
+   
+   
     tax=serializers.SerializerMethodField(method_name='tax_calc')
-
-
+   
+   
     def tax_calc(self,product:Product):
         return product.unit_price*Decimal(1.1)
+
+    '''normal representation of the serializer'''
+    # id=serializers.IntegerField()
+    # title=serializers.CharField(max_length=255)
+    # price=serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
+    
+    # collection=serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-details'
+    # )
+
+    
--- store/views.py
+++ store/views.py
@@ -1,17 +1,28 @@
+import collections
+from os import stat
+from django.db.models.aggregates import Count
+from django.db.models.fields.related import RECURSIVE_RELATIONSHIP_CONSTANT
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers,status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializer import ProductSerializer
+from .models import Collection, Product
+from .serializer import CollectionSerializer, ProductSerializer
 
 
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    query_set=Product.objects.all()
-    serializer=ProductSerializer(query_set,many=True)
-    return Response(serializer.data)
+    if request.method=='GET':
+        query_set=Product.objects.select_related('collection').all()
+        serializer=ProductSerializer(query_set,many=True,context={'request': request})
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer=ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
 
 '''
 handling the exception in traditional way if objectt does not exist
@@ -25,8 +36,30 @@ def product_details(request,id):
     except Product.DoesNotExist :
         return Response(status=status.HTTP_404_NOT_FOUND)'''
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def product_details(request,id):
-    product=get_object_or_404(Product,pk=id)
-    serializer=ProductSerializer(product)
+    product=get_object_or_404(Product,pk=id)#keeping the object outside for availablitiy on all bolcks
+    if request.method=='GET':  
+        
+        serializer=ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method=='PUT':
+        serializer=ProductSerializer(product,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method=='DELETE':
+        if product.orderitem_set.count()>0:
+            return Response(
+                {"error":'item cant be deleted cuz its ordered'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+
+@api_view()
+def collection_details(request,pk):
+    collection=Collection.objects.get(pk=pk)
+    serializer=CollectionSerializer(collection)
     return Response(serializer.data)
