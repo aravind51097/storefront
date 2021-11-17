f4d0900236f1c37759693d886186badcc7ec05ae
aravind51097 <aravind51097@gmail.com>
2021-11-17 18:30:57 +0530

    commit

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+
+from store.models import Product
 from . import views
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-router=DefaultRouter()
-router.register('product',views.ProductViewSet)
+router=routers.DefaultRouter()
+router.register('products',views.ProductViewSet)
 router.register('collection',views.CollectionViewSet)
+products_router=routers.NestedDefaultRouter(router,'products',lookup='product')
+products_router.register('reviews',views.ReviewViewSet,basename='product-reviews')
 
 
-urlpatterns = router.urls
+
+urlpatterns = router.urls+products_router.urls
